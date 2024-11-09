5e7c6bd58d5849835794742e95b3db0a84b001a8
gabster-code <gabstermedia@gmail.com>
2024-11-09 12:39:51 +0800

    fix: reset password page and remove unused routes

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -6,13 +6,15 @@ interface ResetPasswordPageProps {
   };
 }
 
-export default function ResetPasswordPage({
+export default async function ResetPasswordPage({
   searchParams,
 }: ResetPasswordPageProps) {
+  const token = await Promise.resolve(searchParams.token);
+
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-        <ResetPasswordForm token={searchParams.token} />
+        <ResetPasswordForm token={token} />
       </div>
     </div>
   );
