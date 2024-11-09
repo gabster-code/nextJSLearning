ca813cd7e88053c57b3adf4534ae5376a924e9d9
gabster-code <gabstermedia@gmail.com>
2024-11-09 20:27:06 +0800

    fix: resolution error

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "./lib/db";
+import { prisma } from "@/lib/db";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import type { NextAuthConfig } from "next-auth";
