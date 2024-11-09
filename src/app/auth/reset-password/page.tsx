2b6b40d8466769005d21762416425c4a8a26a711
gabster-code <gabstermedia@gmail.com>
2024-11-09 13:17:35 +0800

    fix: ye

--- src/app/auth/reset-password/page.tsx
+++ src/app/auth/reset-password/page.tsx
@@ -1,19 +1,18 @@
 import { ResetPasswordForm } from "@/components/auth/reset-password-form";
-import type { SearchParams } from "next/navigation";
 
 interface ResetPasswordPageProps {
-  searchParams: SearchParams;
+  searchParams: { [key: string]: string | string[] | undefined };
 }
 
 export default async function ResetPasswordPage({
   searchParams,
 }: ResetPasswordPageProps) {
-  const token = searchParams.get("token");
+  const token = searchParams.token as string | undefined;
 
   return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-        <ResetPasswordForm token={token || undefined} />
+        <ResetPasswordForm token={token} />
       </div>
     </div>
   );
