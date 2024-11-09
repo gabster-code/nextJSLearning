9c1b522d7259a5c1f311f9cadfbd76567181c2d0
gabster-code <gabstermedia@gmail.com>
2024-11-09 20:44:10 +0800

    add: development banner header

--- src/components/development-banner.tsx
+++ src/components/development-banner.tsx
@@ -3,7 +3,7 @@
 export function DevelopmentBanner() {
   return (
     <div className="bg-yellow-500 text-black px-4 py-1 text-center text-sm">
-      🚧 Development Environment 🚧
+      🚧 Development Environment!! 🚧
     </div>
   );
 }
